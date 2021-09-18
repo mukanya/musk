b61217050ea531b3a9af7366a5dc410e9f652e38
Brian <briangondo@gmail.com>
2021-09-17 21:42:35 -0700

    "second commit'

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,15 @@
 import firebase from "firebase";
 
+console.log('kuno')
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDiTuojajitDHaWj4PlnE6MGZWJlyJx3D8",
-  authDomain: "muskacademy-610b0.firebaseapp.com",
-  projectId: "muskacademy-610b0",
-  storageBucket: "muskacademy-610b0.appspot.com",
-  messagingSenderId: "317696062754",
-  appId: "1:317696062754:web:b6809b5fe4f6a2ebdd217b",
+  
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-VCHVRDXJ4F"
 };
 
@@ -19,3 +22,25 @@ const storage = firebase.storage();
 
 export { auth, provider, storage, db};
 export default db;
+
+
+// import firebase from "firebase";
+
+// const firebaseConfig = {
+//   apiKey: "process.env.REACT_APP_API_KEY",
+//   authDomain: "process.env.REACT_APP_AUTH_DOMAIN",
+//   projectId: "process.env.REACT_APP_PROJECT_ID",
+//   storageBucket: "process.env.REACT_APP_STORAGE_BUCKET",
+//   messagingSenderId: "process.env.REACT_APP_MESSAGE_ID",
+//   appId: "1:317696062754:web:b6809b5fe4f6a2ebdd217b",
+//   measurementId: "G-VCHVRDXJ4F"
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// const provider = new firebase.auth.GoogleAuthProvider();
+// const storage = firebase.storage();
+
+// export { auth, provider, storage, db};
+// export default db;
